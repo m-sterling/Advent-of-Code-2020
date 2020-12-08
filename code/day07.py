2c68a1ebb5d58e4ec3237ca218d06049466f1f81
Morgan Rose Sterling <morgan.rose.sterling@gmail.com>
2020-12-08 02:08:19 -0500

    Day 7 (not done)
    Added temporary placeholder for day 7

---
+++ code/day07.py
@@ -0,0 +1,15 @@
+# note: this day is (obviously) NOT complete
+# it is merely a placeholder so that I remember to come back and complete this day
+def main():
+    with open('../inputs/day07', 'r') as f:
+        src = [line.strip() for line in f.read().split('\n') if line]
+    print(part1(src))
+    print(part2(src))
+
+def part1(src):
+    return -1
+
+def part2(src):
+    return -1
+
+main()
